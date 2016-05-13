80bd84836bc3551f83234640f5b17bb61f668181
Mark <mark@eitghtahead.de>
2016-05-13 16:18:11 +0200

    x

--- client/app.ts
+++ client/app.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import 'zone.js/dist/zone';
-import {Component,NgZone, OnInit } from 'angular2/core';
+import {Component,NgZone, OnInit } from '@angular/core';
 import {bootstrap} from 'angular2-meteor-auto-bootstrap';
 import {Parties} from '../collections/parties';
 import {Tracker} from 'meteor/tracker';
@@ -13,13 +13,13 @@ import {Mongo} from "meteor/mongo";
 class Socially implements OnInit {
 	collection: Mongo.Cursor<Object>;
 	parties;
-	
+
 	constructor () {
 		this.collection=Parties.find({});
 	}
 
 	ngOnInit(){
-		
+
 		this.parties=this.collection.fetch().values();
 		// console.log(this.collection.fetch().
 	}
