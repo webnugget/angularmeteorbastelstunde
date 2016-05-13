41ad4fd1d1123527a2adefb9416a50509976fba7
webnugget <info@webnugget.de>
2016-05-13 15:55:15 +0200

    update

--- client/app.ts
+++ client/app.ts
@@ -5,22 +5,32 @@ import {bootstrap} from 'angular2-meteor-auto-bootstrap';
 import {Parties} from '../collections/parties';
 import {Tracker} from 'meteor/tracker';
 import {Mongo} from "meteor/mongo";
+import {MeteorComponent} from 'angular2-meteor';
 
 @Component({
 	selector: 'app',
 	templateUrl: 'client/app.html'
 })
-class Socially implements OnInit {
-	collection: Mongo.Cursor<Object>;
-	parties;
+class Socially extends MeteorComponent implements OnInit  {
+	parties: Mongo.Cursor<any>;
 	
 	constructor () {
-		this.collection=Parties.find({});
+		super();
+		console.log(Parties);
+		
 	}
 
 	ngOnInit(){
-		
-		this.parties=this.collection.fetch().values();
+
+		this.autorun(() => {
+			console.log('autorun');
+			this.subscribe('parties', () => {
+				this.parties = Parties.find({});
+				console.log('joschi');
+			}, true);
+		}, true);
+	 	// console.log(this.parties.fetch());	
+		// this.parties=this.collection.fetch().values();
 		// console.log(this.collection.fetch().
 	}
 
